5278cb7c111b4eefb385402be95b4c807a6a7dfe
Martin Mucha <martin.mucha.3@student.tuke.sk>
2021-07-23 17:51:05 +0200

    initial commit

---
+++ react-smalltalk/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <div>This is a message board</div>
+      <div>This is a user list</div>
+      <div>This is a message box</div>
+    </div>
+  );
+}
+
+export default App;
