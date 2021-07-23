3a4a0983abd8433282a56235fdc8bd158bccbf70
Juraj Trappl <juro.trappl@gmail.com>
2021-07-23 21:01:59 +0200

    server is functional

--- server/app.js
+++ server/app.js
@@ -1,40 +1,24 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
 const logger = require('morgan');
-require('dotenv').config();
 
-// DB setup
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('Connected to the database!'))
-  .catch(err => {
-    console.log('Problem with connection to the database:', err);
-    process.exit();
-  });
-mongoose.Promise = global.Promise;
+dotenv.config({
+  path: path.resolve(`${__dirname}/../config/.env`)
+});
 
 // Express setup
 const app = express();
+
 app.use(helmet());
 app.use(logger('dev'));
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({
     extended: true
 }));
 
 // Apply routes
 require('./routes')(app);
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 
 module.exports = app
--- server/messages/message.controller.js
+++ server/messages/message.controller.js
@@ -1,4 +1,4 @@
-const Message = require('../models/message');
+const Message = require('./message.model');
 
 // Returns all messages
 function findAll (req, res) {
