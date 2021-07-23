83391524c15381eb7e33e59b436dbcd6b2bffe4a
Juraj Trappl <juro.trappl@gmail.com>
2021-07-23 20:40:08 +0200

    deleted/renamed backend stuff

--- routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const indexRouter = express.Router();
-
-// logic (controller)
-const { findAll } = require('../controllers/message.controller');
-
-indexRouter.get('/messages', findAll);
-
-module.exports = indexRouter;
