a1daa28c8e5d99f43e44451696257f7fbe43df79
Juraj Trappl <juro.trappl@gmail.com>
2021-07-24 01:35:09 +0200

    fixed backend + frontend

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+
+dotenv.config({
+  path: path.resolve(`${__dirname}/../config/.env`)
+});
+
+// DB setup
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Database connected.'))
+  .catch(err => {
+    if (err) throw err;
+    process.exit();
+  })
+
+// Express setup
+const app = express();
+
+app.use(helmet());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Apply routes
+require('./routes')(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`App is running on port ${PORT}`))
